81fa28155129ea539efbc3128e01a790b58cd1d1
ge86soz <ge68soz@um.de>
2022-07-23 14:36:51 +0200

    Added solution for the BBSE bank smart contract

--- contracts/BBSEToken.sol
+++ contracts/BBSEToken.sol
@@ -7,12 +7,17 @@ contract BBSEToken is ERC20 {
   // Minter state variable
   address public minter;
 
+  modifier onlyMinter {
+    require(msg.sender == minter, "You are not the minter");
+    _;
+  }
+
   /**
   * @dev Calls the ERC20 constructor with {name} and {symbol} values.
   * Sets the minter as the contract deployer.
   */
-  constructor() public payable ERC20("TOKEN NAME", "SYMBOL") {
-    // TODO: Complete the constructor
+  constructor() public payable ERC20("BBSE TOKEN", "BBSE") {
+    minter = msg.sender;
   }
 
   /**
@@ -20,8 +25,9 @@ contract BBSEToken is ERC20 {
   * The minter role can only be passed by the current minter.
   * @param _minter address of the new minter
   */
-  function passMinterRole(address _minter) public {
-    // TODO: Complete the function
+  function passMinterRole(address _minter) public onlyMinter{
+//    require(msg.sender == minter);
+    minter = _minter;
   }
 
 
@@ -32,6 +38,7 @@ contract BBSEToken is ERC20 {
   * @param amount amount of the tokens to be issued
   */
   function mint(address account, uint256 amount) public {
-    // TODO: Complete the function
+    require(msg.sender == minter, "You are not the minter");
+    _mint(account, amount);
   }
 }
